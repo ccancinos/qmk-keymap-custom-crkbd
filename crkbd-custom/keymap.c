b883b531d2d52d44e538a0c7debc41a7b86df857
ccancinos <ccancinos@users.sourceforge.net>
2022-01-16 21:37:07 -0300

    Removing RBG configuration

--- crkbd-custom/keymap.c
+++ crkbd-custom/keymap.c
@@ -3,11 +3,6 @@
 
 extern keymap_config_t keymap_config;
 
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
-
 #ifdef OLED_DRIVER_ENABLE
 static uint32_t oled_timer = 0;
 #endif
@@ -15,6 +10,7 @@ static uint32_t oled_timer = 0;
 
 extern uint8_t is_master;
 
+#define OLED_SCREEN_TIMEOUT 30000
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -27,10 +23,7 @@ enum layers {
 };
 
 // Custom keycodes for layer keys
-// Dual function escape with left command
-#define KC_LGESC LGUI_T(KC_ESC)
-// #define _______ KC_NO
-
+// Dual function left shift and caps lock
 #define KC_STCAP MT(MOD_LSFT, KC_CAPS)
 
 // Add screenshot buttons using macros? https://www.ias.edu/itg/content/keyboard-shortcuts-capture-screen-shot-mac-os-x
@@ -50,10 +43,7 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
-  NAV,
-  RGBRST,
-
-  KC_RACL // right alt / colon
+  NAV
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -113,6 +103,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_270; }
 
 void render_space(void) { oled_write_P(PSTR("     "), false); }
+
+void render_spaces(int count) { for(int i = 0; i < count; i++) { render_space(); } }
 
 void render_mod_status_gui_alt(uint8_t modifiers) {
     static const char PROGMEM gui_off_1[] = {0x85, 0x86, 0};
@@ -333,17 +325,9 @@ void render_wpm(void) {
     oled_write(wpm_str, false);
 }
 
-//void render_status_main(void) {
-//    render_logo();
-//    render_space();
-//    render_layer_state_logo();
-//    render_space();
-//    render_os_modifiers();
-//}
-
 void render_status_main(void) {
     render_logo();
-    render_space(); render_space(); render_space(); render_space(); render_space(); render_space();
+    render_spaces(6);
     render_os_modifiers();
     render_space();
     render_os_modifiers_name();
@@ -353,12 +337,12 @@ void render_status_secondary(void) {
     render_layer_state_logo();
     render_space();
     render_layer_names();
-    render_space(); render_space(); render_space(); render_space(); render_space(); render_space();
+    render_spaces(6);
     render_wpm();
 }
 
 void oled_task_user(void) {
-    if (timer_elapsed32(oled_timer) > 30000) {
+    if (timer_elapsed32(oled_timer) > OLED_SCREEN_TIMEOUT) {
         oled_off();
         return;
     }
@@ -367,7 +351,7 @@ void oled_task_user(void) {
 #endif
 
     if (is_master) {
-        render_status_main();  // Renders the current keyboard state (layer, lock, caps, scroll, etc)
+        render_status_main();
     } else {
         render_status_secondary();
     }
@@ -375,58 +359,8 @@ void oled_task_user(void) {
 #endif
 
 //,------------------------------------------------------------------------------------
-//, LEADER KEYS CONFIGURATION
+//, KEYPRESS CONFIGURATION
 //,------------------------------------------------------------------------------------
-LEADER_EXTERNS();
-
-void matrix_scan_user(void) {
-  LEADER_DICTIONARY() {
-    leading = false;
-    leader_end();
-
-    SEQ_ONE_KEY(KC_H) {
-      // using leader key for quick # sign
-      send_unicode_string("#");
-    }
-    SEQ_TWO_KEYS(KC_K, KC_R) {
-      send_unicode_string("宜しくお願い致します");
-    }
-    // SEQ_TWO_KEYS(KC_D, KC_D) {
-    //   SEND_STRING(SS_LCTL("a") SS_LCTL("c"));
-    // }
-    // SEQ_THREE_KEYS(KC_D, KC_D, KC_S) {
-    //   SEND_STRING("https://start.duckduckgo.com\n");
-    // }
-    // SEQ_TWO_KEYS(KC_A, KC_S) {
-    //   register_code(KC_LGUI);
-    //   register_code(KC_S);
-    //   unregister_code(KC_S);
-    //   unregister_code(KC_LGUI);
-    // }
-  }
-}
-
-//,------------------------------------------------------------------------------------
-//, RGB CONFIGURATION
-//,------------------------------------------------------------------------------------
-
-int RGB_current_mode;
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
-
-void matrix_init_user(void) {
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
@@ -434,79 +368,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
     // set_timelog();
   }
-  static uint16_t my_colon_timer;
 
-  switch (keycode) {
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_SYMBOLS);
-        update_tri_layer_RGB(_SYMBOLS, _NAV, _ADJUST);
-      } else {
-        layer_off(_SYMBOLS);
-        update_tri_layer_RGB(_SYMBOLS, _NAV, _ADJUST);
-      }
-      return false;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_NAV);
-        update_tri_layer_RGB(_SYMBOLS, _NAV, _ADJUST);
-      } else {
-        layer_off(_NAV);
-        update_tri_layer_RGB(_SYMBOLS, _NAV, _ADJUST);
-      }
-      return false;
-    case ADJUST:
-      if (record->event.pressed) {
-        layer_on(_ADJUST);
-      } else {
-        layer_off(_ADJUST);
-      }
-      return false;
-    case NAV:
-      if (record->event.pressed) {
-        layer_on(_NAV);
-      } else {
-        layer_off(_NAV);
-      }
-      return false;
-    case KC_RACL:
-      if (record->event.pressed) {
-        my_colon_timer = timer_read();
-        register_code(KC_RALT);
-      } else {
-        unregister_code(KC_RALT);
-        if (timer_elapsed(my_colon_timer) < TAPPING_TERM) {
-          SEND_STRING(":"); // Change the character(s) to be sent on tap here
-        }
-      }
-      return false;
-    case RGBRST:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgblight_default();
-          rgblight_enable();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
-      #ifdef RGB_MATRIX_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgb_matrix_default();
-          rgb_matrix_enable();
-        }
-      #endif
-      break;
-  }
   return true;
 }
-
-#ifdef RGB_MATRIX_ENABLE
-
-void suspend_power_down_keymap(void) {
-    rgb_matrix_set_suspend_state(true);
-}
-
-void suspend_wakeup_init_keymap(void) {
-    rgb_matrix_set_suspend_state(false);
-}
-
-#endif
