b19a68717e302a081341cd8eaf3949fd3d970f1d
ccancinos <ccancinos@users.sourceforge.net>
2022-05-05 20:30:40 -0300

    Moving Caps Lock to Ctrl key insteado of Shift

--- crkbd-custom/keymap.c
+++ crkbd-custom/keymap.c
@@ -25,6 +25,8 @@ enum layers {
 // Custom keycodes for layer keys
 // Dual function left shift and caps lock
 #define KC_STCAP MT(MOD_LSFT, KC_CAPS)
+// Dual function left control and caps lock
+#define KC_CTCAP MT(MOD_LCTL, KC_CAPS)
 
 // Add screenshot buttons using macros? https://www.ias.edu/itg/content/keyboard-shortcuts-capture-screen-shot-mac-os-x
 // Screenshot screen on file
@@ -51,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_STCAP,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+     KC_CTCAP,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,MO(_SYMBOLS),KC_SPC,   KC_ENT,MO(_NAV), KC_RALT
                                       //`--------------------------'  `--------------------------'
